48dcc37d96562238fb9b4364e5a05827f4fad476
or-agami <oragami0@gmail.com>
2022-11-06 19:36:28 +0200

    update contact me, deploy test done

--- src/cmps/contact-me.tsx
+++ src/cmps/contact-me.tsx
@@ -1,20 +1,19 @@
 import emailjs from '@emailjs/browser';
-import { DetailedHTMLProps, FormEvent, FormHTMLAttributes, useRef } from 'react';
+import { FormEvent, useRef } from 'react';
 
 export const ContactMe = () => {
 
-  const form = useRef<any>();
+  const form = useRef<HTMLFormElement>(null)
 
   const sendEmail = (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log('sendEmail');
-    
-      emailjs.sendForm(process.env.REACT_APP_EMAILJS_SERVICE_ID || '', process.env.REACT_APP_EMAILJS_TEMPLATE_ID || '', form.current, process.env.REACT_APP_EMAILJS_PUBLIC_KEY)
-        .then((result) => {
-          console.log(result.text);
-        }, (error) => {
-          console.log(error.text);
-        });
+    event.preventDefault()
+    console.log('Sending Email')
+    emailjs.sendForm(process.env.REACT_APP_EMAILJS_SERVICE_ID || '', process.env.REACT_APP_EMAILJS_TEMPLATE_ID || '', form.current || '', process.env.REACT_APP_EMAILJS_PUBLIC_KEY)
+      .then((result) => {
+        console.log(result.text)
+      }, () => {
+        console.error('Cannot send email, please try again later')
+      })
   };
 
   return (
