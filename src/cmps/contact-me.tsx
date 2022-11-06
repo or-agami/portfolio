2a646d60f0d6e50725fe9320ebbd983e73d1b426
or-agami <oragami0@gmail.com>
2022-11-06 18:31:56 +0200

    deploy test2

--- src/cmps/contact-me.tsx
+++ src/cmps/contact-me.tsx
@@ -1,6 +1,8 @@
 
 export const ContactMe = () => {
 console.log('process.env.REACT_APP_EMAILJS_KEY:', process.env.REACT_APP_EMAILJS_KEY)
+const key = process.env.REACT_APP_EMAILJS_KEY
+console.log('key:', key)
   return (
     <section className="contact-me">
       <h1>Contact Me Works!</h1>
