3545875335e505afb0e4acc4b13f116c376e8124
or-agami <oragami0@gmail.com>
2022-11-10 21:57:04 +0200

    update project swiper, need clean up

--- src/cmps/project-swiper.tsx
+++ src/cmps/project-swiper.tsx
@@ -7,6 +7,8 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation, Pagination, Scrollbar, A11y, Autoplay } from 'swiper';
 import { BiLinkExternal } from "react-icons/bi";
 import { AiFillTag } from "react-icons/ai";
+import { useEffect, useRef, useState } from 'react';
+import { useInView } from 'react-intersection-observer';
 
 export const ProjectSwiper = () => {
 
@@ -18,9 +20,10 @@ export const ProjectSwiper = () => {
         slidesPerView={1}
         navigation
         pagination={{ clickable: true }}
-      rewind={true}
-      autoplay={{ pauseOnMouseEnter: true, disableOnInteraction: false }}
+        rewind={true}
+        autoplay={{ pauseOnMouseEnter: true, disableOnInteraction: false }}
       >
+        {/* <ProjectPreviewTest project={projects[0]} /> */}
         {projects.map((project: Project) =>
           <SwiperSlide>
             <ProjectPreview key={project.id} project={project} />
@@ -33,17 +36,65 @@ export const ProjectSwiper = () => {
 
 const ProjectPreview = ({ project }: { project: Project }) => {
 
+  const { ref, inView } = useInView({
+    threshold: 1,
+  })
+
+  const [thumbnail, setThumbnail] = useState(project.thumbnail)
+
+  const videoRef = useRef<HTMLVideoElement | null>(null)
+
+  useEffect(() => {
+    inView ? playVideo() : pauseVideo()
+  }, [inView])
+
+  const playVideo = () => {
+    console.log('playVideo:', )
+    if (videoRef.current) videoRef.current.play()
+  }
+
+  const pauseVideo = () => {
+    console.log('pauseVideo:', )
+    if (videoRef.current) videoRef.current.pause()
+  }
+
+  const changeThumbnail = (snapshot: string) => {
+    console.log('snapshot:', snapshot)
+    setThumbnail({ type: 'image', name: snapshot })
+  }
+
+  const setDefaultThumbnail = () => {
+    console.log('setDefaultThumbnail:',)
+    setThumbnail(project.thumbnail)
+    setTimeout(playVideo, 100)
+  }
+
   return (
-    <article key={project.id} className="project-preview">
+    <article key={project.id} className="project-preview-test" ref={ref} onClick={playVideo}>
       <header className="flex align-center justify-center project-header">
         <h1 className="title">{project.title} </h1>
         <a className="url" href={project.url} target="_blank" rel="noreferrer"><BiLinkExternal /></a>
       </header>
       <div className="flex details">
         <div className="thumbnail">
-          <img src={require(`../assets/img/portfolio/${project.id}-thumbnail.png`)} alt="" />
+          {/* <img src={require(`../assets/img/portfolio/${project.id}-thumbnail.png`)} alt="" /> */}
+          {thumbnail.type === 'video' ?
+            <div className="phone-thumbnail">
+              <video src={require(`../assets/video/${project.id}/${project.thumbnail.name}.mp4`)} loop ref={videoRef}></video>
+              <img src={require(`../assets/img/iPhone-14-frame.png`)} alt="" />
+            </div>
+            : thumbnail.type === 'image' &&
+            <img className="img-thumbnail" src={require(`../assets/img/portfolio/${project.id}/${thumbnail.name}.png`)} alt={thumbnail.name} title={thumbnail.name} />
+          }
         </div>
         <div className="flex-column info">
+          <div className="snapshots" onMouseLeave={() => setDefaultThumbnail()}>
+            {project.snapshots.map((snapshot, idx) =>
+              <img className={thumbnail.name === snapshot ? 'active' : ''} alt={snapshot} title={snapshot} key={idx}
+                src={require(`../assets/img/portfolio/${project.id}/${snapshot}.png`)}
+                onMouseEnter={() => changeThumbnail(snapshot)} />
+            )}
+          </div>
           <h2 className="subtitle">{project.subtitle}</h2>
           <h4 className="category">Category: <span>{project.category}</span></h4>
           {/* <p className="description">{project.desc}</p> */}
@@ -60,3 +111,33 @@ const ProjectPreview = ({ project }: { project: Project }) => {
     </article>
   )
 }
+
+// const ProjectPreview = ({ project }: { project: Project }) => {
+
+//   return (
+//     <article key={project.id} className="project-preview">
+//       <header className="flex align-center justify-center project-header">
+//         <h1 className="title">{project.title} </h1>
+//         <a className="url" href={project.url} target="_blank" rel="noreferrer"><BiLinkExternal /></a>
+//       </header>
+//       <div className="flex details">
+//         <div className="thumbnail">
+//           <img src={require(`../assets/img/portfolio/${project.id}-thumbnail.png`)} alt="" />
+//         </div>
+//         <div className="flex-column info">
+//           <h2 className="subtitle">{project.subtitle}</h2>
+//           <h4 className="category">Category: <span>{project.category}</span></h4>
+//           {/* <p className="description">{project.desc}</p> */}
+//           <ul className="flex tag-list">
+//             {project.tags.map((tag, idx) =>
+//               <li className="flex align-center tag" key={idx}>
+//                 <span className="icon"><AiFillTag /></span>
+//                 {tag}
+//               </li>
+//             )}
+//           </ul>
+//         </div>
+//       </div>
+//     </article>
+//   )
+// }
--- src/data/project.const.ts
+++ src/data/project.const.ts
@@ -5,6 +5,8 @@ export interface Project {
   desc: string;
   url: string;
   publishedAt: string;
+  thumbnail: { type: 'video' | 'image', name: string };
+  snapshots: string[];
   category: string;
   tags: string[];
   stack?: Array<'HTML' | 'Vanilla JS' | 'React' | 'Svelte' | 'Angular' | 'Vue' | 'NodeJS' | 'ExpressJS' | 'MongoDB' | 'Local Storage'>;
@@ -19,6 +21,8 @@ export const projects: Project[] = [
     desc: 'agenda is an platform where anyone can create and manage the tools they need to run every aspect of their work. With agenda every project become more easy to manage and follow the progress of the project members, open your account and try now!. Inspired by Monday',
     url: 'https://agenda.agami-network.com',
     publishedAt: 'Sep 2022',
+    thumbnail: { type: 'video', name: 'agenda-mobile' },
+    snapshots: ['homepage', 'board', 'kanban', 'dashboard'],
     category: 'Tool',
     tags: ['Organize', 'Productivity tool', 'Management'],
     stack: ['React', 'NodeJS', 'ExpressJS', 'MongoDB'],
@@ -30,31 +34,33 @@ export const projects: Project[] = [
     desc: '"Regular" keyboard layout is the same since the original mechanical type writer. I tried to make a keyboard with minimal fingers movement and improved ergonomics without compromise keys behavior. the layout is fully programmable with easy to use config, PCB design with future proof in mind - hot-swappable switches, easy to swap rechargable battery. you want to get one? click "View Project"!',
     url: 'https://github.com/or-agami/Ergogami',
     publishedAt: 'Jule 2022',
+    thumbnail: { type: 'image', name: 'both-versions' },
+    snapshots: ['both-versions', 'assembled', 'pcb', 'pcb-both'],
     category: 'Hardware',
     tags: ['Electronics', 'Keyboard'],
   },
-  {
-    id: 'meme-generator',
-    title: 'Meme Generator',
-    subtitle: 'Single page web-app to make awesome memes',
-    desc: 'Awesome looking single web app with meme templates and fully customizable pics with saving, download and share options, build with vanilla js and responsive design ',
-    url: 'https://or-agami.github.io/meme-generator-ca/',
-    publishedAt: 'Aug 2022',
-    category: 'Game',
-    tags: ['Game', 'Canvas', 'Meme'],
-    stack: ['HTML', 'Vanilla JS', 'Local Storage'],
-  },
-  {
-    id: 'minesweeper',
-    title: 'Minesweeper',
-    subtitle: 'Minesweeper game with extra features',
-    desc: 'Minesweeper is single-player logic-based computer game played on rectangular board whose object is to locate a predetermined number of randomly-placed mines in the shortest possible time by clicking on safe squares while avoiding the squares with mines.',
-    url: 'https://or-agami.github.io/Minesweeper-CA/',
-    publishedAt: 'Jule 2022',
-    category: 'Game',
-    tags: ['Matrixes', 'Game'],
-    stack: ['HTML', 'Vanilla JS'],
-  },
+  // {
+  //   id: 'meme-generator',
+  //   title: 'Meme Generator',
+  //   subtitle: 'Single page web-app to make awesome memes',
+  //   desc: 'Awesome looking single web app with meme templates and fully customizable pics with saving, download and share options, build with vanilla js and responsive design ',
+  //   url: 'https://or-agami.github.io/meme-generator-ca/',
+  //   publishedAt: 'Aug 2022',
+  //   category: 'Game',
+  //   tags: ['Game', 'Canvas', 'Meme'],
+  //   stack: ['HTML', 'Vanilla JS', 'Local Storage'],
+  // },
+  // {
+  //   id: 'minesweeper',
+  //   title: 'Minesweeper',
+  //   subtitle: 'Minesweeper game with extra features',
+  //   desc: 'Minesweeper is single-player logic-based computer game played on rectangular board whose object is to locate a predetermined number of randomly-placed mines in the shortest possible time by clicking on safe squares while avoiding the squares with mines.',
+  //   url: 'https://or-agami.github.io/Minesweeper-CA/',
+  //   publishedAt: 'Jule 2022',
+  //   category: 'Game',
+  //   tags: ['Matrixes', 'Game'],
+  //   stack: ['HTML', 'Vanilla JS'],
+  // },
   // {
   //   id: 'touch-nums',
   //   name: 'Touch Nums',
