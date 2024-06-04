cfd301c83da93e546931e05b64b59a50f8e24ccd
Or Agami <57838085+or-agami@users.noreply.github.com>
2024-06-04 13:05:11 +0300

    Update skills.const.tsx

--- src/data/skills.const.tsx
+++ src/data/skills.const.tsx
@@ -25,11 +25,9 @@ export const specialties: Specialty[] = [
   },
 ]
 
-
-type Integer1to10 = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10
 interface Skill {
   title: string,
-  score: Integer1to10,
+  score: number,
 }
 export const skills: Skill[] = [
   {
